de85fac18dcbc2362c23fd56587e16da30ddebd8
Carlton Dwayne <carltondgrandison@gmail.com>
2019-06-03 00:55:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,15 +4,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/hey' do
-    @session = session
-  end
-
   get '/' do
 
     @session = session
     erb :index
+  end
 
+  get '/hey' do
+    @session = session
   end
 
 end
